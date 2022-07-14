388f7492f0f2e9c8c8ba9ef8f719176c61e6a1fe
mdugdale <mark.dugdale@bossastudios.com>
2022-07-14 13:42:08 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -52,7 +52,7 @@ main.post("/pets", async (req, res) => {
     //      });
     //    });
     res.setHeader("Access-Control-Allow-Origin", "*");
-    //res.status(200).json(pets);
+    // res.status(200).json(pets);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
