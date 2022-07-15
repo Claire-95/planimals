a2dd7970876fcea70ff0a11f263db294e6f4e780
mdugdale <mark.dugdale@bossastudios.com>
2022-07-15 13:48:52 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -38,7 +38,7 @@ main.get("/pets", async (req, res) => {
 });
 
 // Post a Pet
-main.post("/pets", async (req, res) => {
+app.post("/pets", async (req, res) => {
   try {
     console.log(req.body);
     await db.collection(petCollection).add(req.body);
