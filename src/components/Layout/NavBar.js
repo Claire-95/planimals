b9d347b152102a474ffed916ff347cf57cb0e21b
mdugdale <mark.dugdale@bossastudios.com>
2022-07-20 14:55:07 +0100

    Responsive nav basically working

--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -1,22 +1,25 @@
 import { Outlet } from "react-router-dom";
-
 import classes from "./NavBar.module.css";
 import loginService from "../../services/login-service";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { useRef } from "react";
+import { useState } from "react";
 
 const NavBar = () => {
-  const navRef = useRef();
+  const [navRef, setNavRef] = useState(classes.closed);
 
   const showNavBar = () => {
-    navRef.current.classList.toggle("responsive_nav");
+    if (navRef === classes.closed) {
+      setNavRef(classes.open);
+    } else {
+      setNavRef(classes.closed);
+    }
   };
 
   return (
     <div>
       <header>
         <h3>Planimals</h3>
-        <nav ref={navRef}>
+        <nav className={navRef}>
           <a href="/all-pets">All Pets</a>
           <a href="/new-pet">Add New Pet</a>
           <a href="/fed-pets">Fed Pets</a>
