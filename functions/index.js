12edcbdc0583d76a2effb63084df639d911d5936
mdugdale <mark.dugdale@bossastudios.com>
2022-07-22 11:14:55 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -81,10 +81,8 @@ main.post("/pets", async (req, res) => {
         data: doc.data(),
       });
     });
-    res.status(200).json(pets);
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
   }
 });
 
